7b1d0be80ebbe6f0d07cd719a8421d7781699ac5
Yuanqing Cai <ckjacket@gmail.com>
2016-02-25 00:43:34 +0800

    more testing on react with flask

--- static/js/app.js
+++ static/js/app.js
@@ -10,12 +10,15 @@ import Question from './components/question/index';
 import TestButton from './components/test/index';
 
 const {Button} = WeUI;
+const a = document.getElementById('init_state').innerHTML;
+// const b = JSON.parse(a);
 
 class App extends React.Component {
     render() {
         return (
-          <TestButton/>
-          // <Question/>
+          // <TestButton/>
+          // <Button> {a}</Button>
+          <Question />
         );
     }
 };
