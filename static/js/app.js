8f190a0a9bf9594dff0e46160e7fc11b54fcf407
Yuanqing Cai <ckjacket@gmail.com>
2016-02-26 00:53:39 +0800

    remove unaviable js

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,3 @@
 /**
  * Created by pp on 16/2/21.
  */
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WeUI from 'react-weui';
-import 'weui';
-import Question from './components/question/index';
-import TestButton from './components/test/index';
-
-const {Button} = WeUI;
-const a = document.getElementById('init_state').innerHTML;
-// const b = JSON.parse(a);
-
-class App extends React.Component {
-    render() {
-        return (
-          // <TestButton/>
-          // <Button> {a}</Button>
-          <Question />
-        );
-    }
-};
-
-ReactDOM.render((
-    <App/>
-), document.getElementById('content'));
